deb83543d97a0dfcaa24105ba5992e5a54251dad
tcklpl <negroniluan@gmail.com>
2025-02-05 19:04:37 -0300

    initial wiki page component

---
+++ game/src/components/menu/wiki/wiki.tsx
@@ -0,0 +1,12 @@
+import type { FunctionComponent } from 'react';
+import { useTranslation } from 'react-i18next';
+
+interface WikiPageProps {}
+
+const WikiPage: FunctionComponent<WikiPageProps> = () => {
+    const { t } = useTranslation([]);
+
+    return <></>;
+};
+
+export default WikiPage;
