6c07bfb32b030b7c5e2ff3a794fd28a7b3d1265a
tcklpl <negroniluan@gmail.com>
2025-02-05 18:55:34 -0300

    territory naming fixes

--- protocol/src/protocol/data/territory.ts
+++ protocol/src/protocol/data/territory.ts
@@ -1,6 +1,48 @@
-
-export type ContinentCode = "n_america" | "s_america" | "europe" | "oceania" | "africa" | "asia";
-export type TerritoryCode = "alaska" | "california" | "cuba" | "greenland" | "labrador" | "mackenzie" | "mexico" | "new_york" | "ottawa" | "vancouver" | "argentina" | "brazil" | "chile" | "venezuela" | "germany" | "france" | "england" | "iceland" | "moscow" | "sweden" | "australia" | "sumatra" | "borneo" | "n_guinea" | "egypt" | "algeria" | "sudan" | "congo" | "s_africa" | "madagascar" | "poland" | "aral" | "india" | "ornsk" | "middle_east" | "vietnam" | "china" | "mongolia" | "dudinka" | "tchita" | "siberia" | "vladvostok" | "japan";
+export type ContinentCode = 'north_america' | 'south_america' | 'europe' | 'oceania' | 'africa' | 'asia';
+export type TerritoryCode =
+    | 'alaska'
+    | 'california'
+    | 'cuba'
+    | 'greenland'
+    | 'labrador'
+    | 'mackenzie'
+    | 'mexico'
+    | 'new_york'
+    | 'ottawa'
+    | 'vancouver'
+    | 'argentina'
+    | 'brazil'
+    | 'chile'
+    | 'venezuela'
+    | 'germany'
+    | 'france'
+    | 'england'
+    | 'iceland'
+    | 'moscow'
+    | 'sweden'
+    | 'australia'
+    | 'sumatra'
+    | 'borneo'
+    | 'new_guinea'
+    | 'egypt'
+    | 'algeria'
+    | 'sudan'
+    | 'congo'
+    | 'south_africa'
+    | 'madagascar'
+    | 'poland'
+    | 'aral'
+    | 'india'
+    | 'ornsk'
+    | 'middle_east'
+    | 'vietnam'
+    | 'china'
+    | 'mongolia'
+    | 'dudinka'
+    | 'tchita'
+    | 'siberia'
+    | 'vladvostok'
+    | 'japan';
 
 export interface GraphNodeTerritoryPacket {
     code: TerritoryCode;
